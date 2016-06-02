22a26584bce5fefb3644dfb304d27c0f3471de4c
housamaldeen <housam993@gmail.com>
2016-06-02 18:53:25 +0300

    Return a mapped array of the input array with a certain function applied to it

--- src/underbar.js
+++ src/underbar.js
@@ -112,6 +112,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var newArray = [];
+    _.each(collection, function (element, i) {
+        newArray.push(iterator(element));
+    })
+    return newArray;
   };
 
   /*
